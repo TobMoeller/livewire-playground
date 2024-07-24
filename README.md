4f12040f9864803ca49ce9491e92efa2db977430
TobMoeller <tobiasmoellerw@t-online.de>
2024-07-24 23:54:59 +0200

    link form wizard

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Tobi's Livewire Playground
 
 Repository to test out component ideas
+
+## Components
+- [Multistep Form Wizard with Livewire Forms as state](https://github.com/TobMoeller/livewire-playground/blob/main/app/Livewire/FormWizard/Wizard.php)
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('test.wizard') }}" :active="request()->routeIs('test.wizard')">
+                        {{ __('Form Wizard') }}
+                    </x-nav-link>
                 </div>
             </div>
 
